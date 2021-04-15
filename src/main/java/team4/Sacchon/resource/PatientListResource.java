a99580e7ce36ed5f3d7888cc191e97d5b152e141
Christos Kourounis <chriskourounis@gmail.com>
2021-04-15 15:29:04 +0300

    Add a post method to the PatientListResource and some minor refactoring in the get method.

--- src/main/java/team4/Sacchon/resource/PatientListResource.java
+++ src/main/java/team4/Sacchon/resource/PatientListResource.java
@@ -1,6 +1,7 @@
 package team4.Sacchon.resource;
 
 import org.restlet.resource.Get;
+import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
 import team4.Sacchon.jpautil.JpaUtil;
 import team4.Sacchon.model.Patient;
@@ -17,12 +18,23 @@ public class PatientListResource extends ServerResource {
     @Get("json")
     public List<PatientRepresentation> getPatient(){
         EntityManager em = JpaUtil.getEntityManager();
-        PatientRepository productRepository = new PatientRepository(em);
-        List<Patient> patients = productRepository.findAll();
+        List<Patient> patients = new PatientRepository(em).findAll();
         em.close();
 
         return patients.stream()
         .map(PatientRepresentation::new)
         .collect(Collectors.toList());
+    }
+
+    @Post("json")
+    public PatientRepresentation add(PatientRepresentation patientRepresentation){
+        if (patientRepresentation == null || patientRepresentation.getName() == null)
+            return null;
+
+        Patient patient = patientRepresentation.createPatient();
+        EntityManager em = JpaUtil.getEntityManager();
+        new PatientRepository(em).save(patient);
+        em.close();
+        return new PatientRepresentation(patient);
     }
 }
