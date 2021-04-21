be4f29d2bbd5db23bc8b3ca1078ad6186025d1de
Christos Kourounis <chriskourounis@gmail.com>
2021-04-21 13:21:01 +0300

    Refactoring the call to get Patient Measurements

--- src/main/java/team4/Sacchon/resource/PatientMeasurementListResource.java
+++ src/main/java/team4/Sacchon/resource/PatientMeasurementListResource.java
@@ -55,57 +55,35 @@ public class PatientMeasurementListResource extends ServerResource {
         return new ApiResult<>(measurementRepresentation, 200, "Measurement has been added");
     }
 
-    /**
-     * Returns all past patient measurements if no query parameters have been passed.
-     * Otherwise it uses the fromDate, toDate and type parameters to call the corresponding queries.
-     */
     @Get("json")
     public ApiResult<Object> getPatientMeasurements() {
-        ApiResult<Object> privileges = checkPatientPrivileges();
-        if (privileges != null)
-            return privileges;
-
         Date fromDate = null;
         Date toDate = null;
-        String type = null;
 
         try {
             fromDate = new SimpleDateFormat("dd/MM/yyyy").parse(getQueryValue("fromDate"));
             toDate = new SimpleDateFormat("dd/MM/yyyy").parse(getQueryValue("toDate"));
-            type = getQueryValue("type");
-
-            if (type != null && (!"carb".equals(type) && !"glucose".equals(type)))
-                return new ApiResult<>(null, 400, "No such data type is logged.");
         } catch (Exception e) {
         }
 
-        EntityManager em = JpaUtil.getEntityManager();
-        Patient patient = new PatientRepository(em).read(id);
-        MeasurementRepository measurementRepository = new MeasurementRepository(em);
-
-        //if not all date info is present return all past measurements
-        if (fromDate == null || toDate == null) {
-            return getAllPatientMeasurements(measurementRepository, patient);
-        }
-        //else return the average glucose or carb for that period
-        else {
-            Double average = 0.0;
-            if (type.equals("glucose")) {
-                average = measurementRepository.getAverageGlucoseOfMeasurements(patient.getId(), fromDate, toDate);
-                return new ApiResult<>(average, 200, "Average glucose levels of patient.");
-            } else {
-                average = measurementRepository.getAverageCarbOfMeasurements(patient.getId(), fromDate, toDate);
-                return new ApiResult<>(average, 200, "Average carb intake levels of patient.");
-            }
-        }
+        return getPatientMeasurements(fromDate, toDate);
     }
 
-    private ApiResult<Object> getAllPatientMeasurements(MeasurementRepository measurementRepository, Patient patient) {
-        List<Measurement> measurements = measurementRepository.getMeasurementsOf(patient.getId());
+    private ApiResult<Object> getPatientMeasurements(Date fromDate, Date toDate) {
+        EntityManager em = JpaUtil.getEntityManager();
+        MeasurementRepository measurementRepository = new MeasurementRepository(em);
+        List<Measurement> measurements;
+        if (fromDate == null || toDate == null) {
+            measurements = measurementRepository.getMeasurementsOf(id);
+        }else {
+            measurements = measurementRepository.getMeasurementsOfIdBetween(id, fromDate, toDate);
+        }
+
         List<MeasurementRepresentation> measurementRepresentationList = new ArrayList<>();
         for (Measurement m : measurements) {
             measurementRepresentationList.add(new MeasurementRepresentation(m));
         }
+        em.close();
         return new ApiResult<>(measurementRepresentationList, 200, "Past patient measurements");
     }
 
